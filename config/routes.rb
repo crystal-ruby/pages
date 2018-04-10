fca4337a59cc2592f265d342dc2fd576250a9dce
Torsten Ruger <torsten@villataika.fi>
2018-04-10 20:14:41 +0300

    move content to pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'high_voltage/pages#show' , id: 'index'
+
+  get "/blog" , to: "blog#index" , as: :blog_index
+  get "/blog/*title" , to: "blog#page" , as: :blog_page
+
 end
