c143b5bbd04c441428220ccf3b5aa30698c60cb8
Torsten Ruger <torsten@villataika.fi>
2018-04-10 22:48:23 +0300

    fix blog index and links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def post_link(index)
+    post = Post.posts.values[index]
+    return "" unless post
+    link = post.date.to_s + "  "
+    link +=  link_to( post.title , blog_post_url(post.slug))
+    link.html_safe
+  end
 end
