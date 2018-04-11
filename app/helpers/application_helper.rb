b1614dc3530a2167a4977075d9b0bada6e974457
Torsten Ruger <torsten@villataika.fi>
2018-04-11 10:45:50 +0300

    start architecture menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,17 @@ module ApplicationHelper
     link +=  link_to( post.title , blog_post_url(post.slug))
     link.html_safe
   end
+
+
+  def ext_link(name = nil, options = nil, html_options = nil, &block)
+    target_blank = {target: "_blank"}
+    if block_given?
+      options ||= {}
+      options = options.merge(target_blank)
+    else
+      html_options ||= {}
+      html_options = html_options.merge(target_blank)
+    end
+    link_to(name, options, html_options, &block)
+  end
 end
